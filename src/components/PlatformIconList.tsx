b29490e09a7365c8266641b2f3ea628a399daffc
AndrewC <andrewc@marvin.com>
2024-03-20 13:29:01 -0500

    Adding PlatformIcons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,35 @@
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+import { HStack, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib';
+
+interface Props {
+    platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        android: FaAndroid,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        mac: FaApple,
+        nintendo: SiNintendo,
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        web: BsGlobe,
+        xbox: FaXbox,
+    };
+
+    return (
+        <HStack marginTop={'1rem'}>
+            {platforms.map((platform) => (
+                <Icon color={'gray.500'} as={iconMap[platform.slug]} />
+            ))}
+        </HStack>
+    );
+};
+
+export default PlatformIconList;
