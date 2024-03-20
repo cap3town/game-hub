541d3a569d940fb46422b54ef641e1a170a59574
AndrewC <andrewc@marvin.com>
2024-03-20 15:51:59 -0500

    Creating a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,16 @@
 import { SkeletonText, Text } from '@chakra-ui/react';
-import useGenres, { Genre } from '../hooks/useGenres';
+import useGenres from '../hooks/useGenres';
 
 const GenreList = () => {
-    const { genres, error, isLoading } = useGenres();
+    const { data, error, isLoading } = useGenres();
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
     return (
         <>
-            {error && <Text>{error}</Text>}
+            {error && console.log('Error retrieving Genres')}
 
             <ul>
-                {isLoading && skeletons.map(() => <SkeletonText />)}
-                {genres.map((genre) => (
+                {isLoading && skeletons.map(() => <SkeletonText skeletonHeight="1rem" />)}
+                {data.map((genre) => (
                     <li>{genre.name}</li>
                 ))}
             </ul>
