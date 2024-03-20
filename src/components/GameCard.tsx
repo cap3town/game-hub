9293544ed50686c6edf1562fd05b5e128df311d1
AndrewC <andrewc@marvin.com>
2024-03-20 15:08:41 -0500

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image, Text, VStack } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
@@ -11,14 +11,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card overflow="hidden" borderRadius="10px">
+        <Card width="300px" overflow="hidden" borderRadius={10}>
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
-                <Heading fontSize="xl">{game.name}</Heading>
-                <HStack marginTop="1rem" justifyContent="space-between">
-                    <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
-                    <CriticScore score={game.metacritic} />
-                </HStack>
+                <VStack height="100%" justifyContent="space-between">
+                    <Heading width="100%" fontSize="xl">
+                        {game.name}
+                    </Heading>
+                    <HStack width="100%" marginTop="1rem" justifyContent="space-between">
+                        <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+                        <CriticScore score={game.metacritic} />
+                    </HStack>
+                </VStack>
             </CardBody>
         </Card>
     );
